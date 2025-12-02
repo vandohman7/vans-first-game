29ab4a0eea7d2cec5812a5801b42c57eaffacd75
Evan Alberg <van.alberg@gmail.com>
2025-12-01 22:19:03 -0600

    pause button working

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,19 +1,32 @@
 extends Control
 
-@onready var label: Label = %Label
-	#
 
+@onready var pause_menu: Control = $"."
+
+#@onready var visiblity = pause_menu.get_node("visible")
+
+func _process(delta):
+	print("_process")
+	testtab()
+	
 func resume():
+	print("resume")
 	get_tree().paused = false
 
 func pause():
+	print("pause")
 	get_tree().paused = true
 
+func isPaused():
+	return get_tree().paused == true
+
 func testtab():
-	if Input.is_action_just_pressed("tab") and !get_tree().paused == false:
+	if Input.is_action_just_pressed("esc") and !isPaused():
 		pause()
-	elif Input.is_action_just_pressed("tab") and get_tree().paused == true:
+	elif Input.is_action_just_pressed("esc") and isPaused():
 		resume()
+		
+	visible = isPaused()
 
 
 func _on_resume_pressed():
@@ -27,7 +40,3 @@ func _on_restart_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
-
-
-func _process(delta):
-	testtab()
