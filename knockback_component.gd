7f7bd055e86ee0dfdc311ae885f3e4dea328d6fa
Evan Alberg <van.alberg@gmail.com>
2026-01-13 10:31:06 -0600

    Update knockback_component.gd

--- knockback_component.gd
+++ knockback_component.gd
@@ -1,11 +1,5 @@
 extends Node
+class_name knockback_component
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export_category('Stats')
+@export var knockback_force: float = 10
